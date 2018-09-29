85b4c2080ee5e6d2fee61d62da8a163960b00d51
infoxicator <mobile@developmentsmbp.home>
2018-09-29 21:51:17 +0100

    refactor(): use short-uid for shortcodes

--- createUrl.js
+++ createUrl.js
@@ -1,25 +1,33 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-unresolved
 const uuid = require('uuid/v4');
-const randomstring = require('randomstring');
+const ShortUID = require('short-uid');
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const shortUID = new ShortUID();
 const { URLS_TABLE } = process.env;
 
 module.exports.run = async (event) => {
   const data = JSON.parse(event.body);
+  const { url } = data;
   const params = {
     TableName: URLS_TABLE,
     Item: {
       id: uuid(),
-      url: data.url,
-      shortCode: randomstring.generate(7),
+      url,
+      shortCode: shortUID.randomUUID(7),
       visits: 0,
     },
   };
-
-  await dynamoDB.put(params).promise();
-  return {
-    statusCode: 200,
-    body: JSON.stringify(params.Item),
-  };
+  try {
+    await dynamoDB.put(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' }),
+    };
+  }
 };
