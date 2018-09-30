c584fce6b76b042112c6b63e2da5e75ba2c7584d
infoxicator <mobile@developmentsmbp.home>
2018-09-30 23:01:16 +0100

    refactor(): separate bussiness logic

--- createUrl.js
+++ createUrl.js
@@ -1,29 +1,13 @@
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-unresolved
-const uuid = require('uuid/v4');
-const ShortUID = require('short-uid');
-
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-const shortUID = new ShortUID();
-const { URLS_TABLE } = process.env;
+const UrlFactory = require('./lib/UrlFactory');
 
 module.exports.run = async (event) => {
+  const urlFactory = new UrlFactory();
   const data = JSON.parse(event.body);
   const { url } = data;
-  const params = {
-    TableName: URLS_TABLE,
-    Item: {
-      id: uuid(),
-      url,
-      shortCode: shortUID.randomUUID(7),
-      visits: 0,
-    },
-  };
+
   try {
-    await dynamoDB.put(params).promise();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
+    const response = await urlFactory.createUrl(url);
+    return response;
   } catch (e) {
     return {
       statusCode: 500,
--- getUrl.js
+++ getUrl.js
@@ -1,39 +1,11 @@
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-unresolved
-
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-const { URLS_TABLE } = process.env;
+const UrlFactory = require('./lib/UrlFactory');
 
 module.exports.run = async (event) => {
-  const params = {
-    TableName: URLS_TABLE,
-    IndexName: 'ShortCodeIndex',
-    KeyConditionExpression: 'shortCode = :shortcode',
-    ExpressionAttributeValues: {
-      ':shortcode': event.pathParameters.shortCode,
-    },
-  };
+  const urlFactory = new UrlFactory();
+  const { shortCode } = event.pathParameters;
   try {
-    const result = await dynamoDB.query(params).promise();
-    if (result.Items.length > 0) {
-      result.Items[0].visits += 1;
-      const updatedItem = {
-        TableName: URLS_TABLE,
-        Item: result.Items[0],
-      };
-      await dynamoDB.put(updatedItem).promise();
-      return {
-        statusCode: 302,
-        headers: {
-          Location: result.Items[0].url,
-        },
-        body: null,
-      };
-    }
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ message: 'invalid shortCode url' }),
-    };
+    const response = await urlFactory.getUrl(shortCode);
+    return response;
   } catch (e) {
     return {
       statusCode: 500,
